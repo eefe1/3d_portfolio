2a32be03439490b19f3f60ed175df1ee24652519
ezgi <esgiefe@gmail.com>
2023-07-15 09:47:50 +0200

    added about section

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Frontend Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
